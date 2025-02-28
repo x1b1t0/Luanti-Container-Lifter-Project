3e892e3b1d81035dbc5890a7d2b7cfb6fd0a4751
x1b1t0 <ivaanv23@gmail.com>
2025-02-28 14:49:30 +0000

    PHP

--- Luanti Web/Back end/index.php
+++ Luanti Web/Back end/index.php
@@ -2,7 +2,9 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = escapeshellarg($_POST['name']);
     $playernumbers = escapeshellarg($_POST['playernumbers']);
-    
+    $creativemode = escapeshellarg($_POST['creativemode']);
+    $enable_damage = escapeshellarg($_POST['pvp']);
+
     //Ejecuccion de el script de bash
     $command = "bash /var/www/html/Back\ end/script.sh $name $message";
     $outpout = shell_exec($command);
