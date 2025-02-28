e02ec88d98ae71cbea12e0e488c8f58c9330a585
x1b1t0 <ivaanv23@gmail.com>
2025-02-28 15:36:20 +0100

    Delete Luanti Web/Back End directory

--- Luanti Web/Back End/controllers/serverController.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('child_process');
-
-const crearServidor = (ipServidor, nombreServidor, puertoServidor, callback) => {
-    exec(`podman run -d --name="${nombreServidor}" -p "${puertoServidor}:30000/udp" -e CLI_ARGS="--gameid devtest" luanti:latest`, callback);
-};
-
-module.exports = { crearServidor };
