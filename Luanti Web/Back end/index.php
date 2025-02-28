1a51312b0bc4c7123e956c48ff102f4a1b333153
x1b1t0 <ivaanv23@gmail.com>
2025-02-28 14:55:20 +0000

    PHP

--- Luanti Web/Back end/index.php
+++ Luanti Web/Back end/index.php
@@ -6,8 +6,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $enable_damage = escapeshellarg($_POST['pvp']);
 
     //Ejecuccion de el script de bash
-    $command = "bash /var/www/html/Back\ end/script.sh $name $message";
+    $command = "bash /var/www/html/Backend/containerlifter.sh $name $message";
     $outpout = shell_exec($command);
     echo $outpout;
 }
-?>
+?> 
