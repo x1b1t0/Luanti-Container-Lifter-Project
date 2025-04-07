1324e9f135042cb21d13bf44e684d6c43ee5d6c4
x1b1t0 <ivaanv23@gmail.com>
2025-04-07 18:27:41 +0000

    pepemanu

--- Luanti Web/Back end/showservers.php
+++ Luanti Web/Back end/showservers.php
@@ -1,6 +1,10 @@
 <?php
 $showservers = shell_exec('sudo podman ps -a --format "table {{.Names}}\t{{.Ports}}#"');
 $serversarray = explode("#", $showservers);
-echo $serversarray[1];
+
 //Hacer un bucle que empiece en el 0 el limite es la longitud del array por cada contenedor que hay crea una fila nueva
+foreach ($serversarray as $fila){
+ echo "fila: $fila";
+}
+//Hay que hacer que el HTML coja el php y con esta informacion cree una table bien organizada y estructurada de todos los valores que aqui aaprecen
 ?>
