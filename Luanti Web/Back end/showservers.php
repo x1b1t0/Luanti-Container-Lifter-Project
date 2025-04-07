960cadad626f63e2c9575a41b06cfe1941aae08d
x1b1t0 <ivaanv23@gmail.com>
2025-04-07 18:15:51 +0000

    pepe

--- Luanti Web/Back end/showservers.php
+++ Luanti Web/Back end/showservers.php
@@ -1,5 +1,6 @@
 <?php
-header('Content-Type: application/json');
-$showservers = shell_exec('podman ps -a --format json');
-echo $showservers;
+$showservers = shell_exec('sudo podman ps -a --format "table {{.Names}}\t{{.Ports}}#"');
+$serversarray = explode("#", $showservers);
+echo $serversarray[1];
+//Hacer un bucle que empiece en el 0 el limite es la longitud del array por cada contenedor que hay crea una fila nueva
 ?>
