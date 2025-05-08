f1cacc120be5a4e2b442122a90f3e49936cbb67c
x1b1t0 <ivaanv23@gmail.com>
2025-05-08 18:29:21 +0200

    Update db.php

--- Luanti Web/Back end/db.php
+++ Luanti Web/Back end/db.php
@@ -1,1 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "luanti";
+$password = "fuckluanti";
+$dbname = "luanti";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
 
